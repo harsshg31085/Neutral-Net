0f038cc8d3aab0678627eac10bcc31726f9a149e
Mitesh Jethawa <jethawamitesh@gmail.com>
2025-12-22 21:00:56 +0530

    fixed typo

--- backend/equity_mirror/settings.py
+++ backend/equity_mirror/settings.py
@@ -76,7 +76,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
-    "http://127.0.0.2:3000"
+    "http://127.0.0.1:3000"
 ]
 
 REST_FRAMEWORK = {
